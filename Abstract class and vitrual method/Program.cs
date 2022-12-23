219ed604bbc79210a2262c9e34fd0168489e64a0
kimi <kimi@DESKTOP-HLSO57E>
2022-12-23 18:02:10 +0330

    Add project files.

---
+++ Abstract class and vitrual method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Abstract_class_and_vitrual_method
+{
+    public abstract class AbstractClass
+    {
+        public virtual void ACM()
+        {
+            Console.WriteLine("Default implementation");
+        }
+    }
+        public class SomeClass1 : AbstractClass
+        { }
+
+        public class SomeClass2 : AbstractClass
+        {
+            public override void ACM()
+            {
+                Console.WriteLine("New Default implementation");
+            }
+        }
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            SomeClass1 sc = new SomeClass1();
+            sc.ACM();
+
+            SomeClass2 soc = new SomeClass2();
+            soc.ACM();
+        }
+    }
+}
